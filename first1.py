f4a0c43a0a8d6dfe45c3647694e647a7144b694b
qingni222 <1811917160@qq.com>
2025-06-06 10:26:06 +0800

    Add files via upload

---
+++ first1.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+
+# 定义数据,以便创建数据框
+data = {
+    '月份':['01月', '02月', '03月'],
+    '1号门店':[200, 150, 180],
+    '2号门店':[120, 160, 123],
+    '3号门店':[110, 100, 160],
+}
+# 根据上面创建的data，创建数据框
+df = pd.DataFrame(data)
+# 定义数据框所用的新索引
+index = pd.Series([1, 2, 3,], name='序号')
+# 将新索引应用到数据框上
+df.index = index
+
+st.header("门店数据")
+# 使用write()方法展示数据框
+st.write(df)
+st.header("折线图")
+
+st.subheader("设置x参数")
+# 通过x指定月份所在这一列为折线图的x轴
+st.line_chart(df, x='月份')
+
+
+# 修改df，用月份列作为df的索引，替换原有的索引
+df.set_index('月份', inplace=True)
+
+st.subheader("设置y参数")
+# 通过y参数筛选只显示1号门店的数据
+st.line_chart(df, y='1号门店')
+# 通过y参数筛选只显示2、3号门店的数据
+st.line_chart(df, y=['2号门店','3号门店'])
+
+st.subheader("设置width、height和use_container_width参数")
+# 通过width、height和use_container_width指定折线图的宽度和高度
+st.line_chart(df, width=300, height=300, use_container_width=False)
